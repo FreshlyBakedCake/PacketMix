9d9346458e4f7f24a12df0237c340f88a78328c7
Samuel Shuert <me@thecoded.prof>
2025-05-21 01:44:31 -0400

    fix: Separate out impermanence

--- systems/teal/hardware-configuration.nix
+++ systems/teal/hardware-configuration.nix
@@ -30,12 +30,4 @@
   };
 
   boot.swraid.enable = true;
-
-  clicks.storage.impermanence = {
-    enable = true;
-    devices = {
-      root = "/dev/disk/by-uuid/ab5c2f52-a737-4b29-a505-e3d0b9d0714c";
-      persist = "/dev/md/a1d1:persist";
-    };
-  };
 }
