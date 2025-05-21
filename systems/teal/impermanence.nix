ea30638cfdbf77f87e2fba5037ddde4df15832fe
Samuel Shuert <me@thecoded.prof>
2025-05-21 01:44:31 -0400

    fix: Separate out impermanence

---
+++ systems/teal/impermanence.nix
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2025 FreshlyBakedCake
+#
+# SPDX-License-Identifier: MIT
+
+{
+  clicks.storage.impermanence = {
+    enable = true;
+    devices = {
+      root = "/dev/disk/by-uuid/ab5c2f52-a737-4b29-a505-e3d0b9d0714c";
+      persist = "/dev/md/a1d1:persist";
+    };
+  };
+}
