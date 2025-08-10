317d3fec488656fbef7df0c2bbc8c202ee33feca
Skyler Grey <minion@freshlybakedca.ke>
2025-08-10 12:09:20 +0000

    ci: remote build on midnight
    Our CI jobs are taking way too long and commonly running out of space.
    
    If we can build on midnight, there'll be no need to repeatedly download
    our cache for CI. Additionally, we're much less likely to run out of
    space

---
+++ systems/midnight/remoteBuilds.nix
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2025 FreshlyBakedCake
+#
+# SPDX-License-Identifier: MIT
+{
+  users.users.remoteBuilds.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOCmfKIvQ6ZClOWi3jHKC8HTQ5Ewo4Kzw6VCJCzrn4ih ci"
+  ];
+}
