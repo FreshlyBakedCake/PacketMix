2459400c125df4aad3d7176186cd76e77223e89d
Skyler Grey <minion@freshlybakedca.ke>
2026-01-03 12:14:31 +0000

    refactor(*): add nilla/etc. in top level directory
    Our josh files and combined project pieces have been a mess for a while.
    As I'm creating a new project, I thought it was high time to tidy them
    up so they pull out the right files and we can share things like our
    inputs.

--- nilla.nix
+++ nilla.nix
@@ -6,125 +6,38 @@ let
   pins = import ./npins;
 
   nilla = import pins.nilla;
-in
-nilla.create (
-  { config, lib }:
-  {
-    includes = [
-      ./homes
-      ./inputs.nix
-      ./lib
-      ./modules
-      ./packages
-      ./systems
-      "${pins.nilla-nixos}/modules/nixos.nix" # We can't use config.inputs here without infinitely-recursing
+
+  settings = config: {
+    nixpkgs = {
+      configuration.allowUnfree = true;
+      overlays = [
+        config.inputs.fenix.result.overlays.default
+      ];
+    };
+    "nixos-24.11" = (settings config).nixpkgs;
+    nixos-unstable = (settings config).nixpkgs;
+  };
+
+  result = (nilla.create [ ]).extend {
+    modules = [
+      ./project.nix
+      (
+        { config, ... }:
+        {
+          config.inputs = builtins.mapAttrs (name: value: {
+            src = value;
+            settings = (settings config).${name} or config.lib.constants.undefined;
+          }) pins;
+        }
+      )
     ];
 
-    config = {
-      packages.allNixOSSystems = {
-        systems = [ "x86_64-linux" ];
-
-        package =
-          { stdenv }:
-          stdenv.mkDerivation {
-            name = "all-nixos-systems";
-
-            dontUnpack = true;
-
-            buildPhase = ''
-              mkdir -p $out
-            ''
-            + (builtins.concatStringsSep "\n" (
-              config.lib.attrs.mapToList (
-                name: value: ''ln -s "${value.result.config.system.build.toplevel}" "$out/${name}"''
-              ) config.systems.nixos
-            ));
-          };
-      };
-
-      packages.allHomes = {
-        systems = [ "x86_64-linux" ];
-
-        package =
-          { stdenv }:
-          stdenv.mkDerivation {
-            name = "all-homes";
-
-            dontUnpack = true;
-
-            buildPhase = ''
-              mkdir -p $out
-            ''
-            + (builtins.concatStringsSep "\n" (
-              config.lib.attrs.mapToList (
-                name: value:
-                ''ln -s "${value.result.${stdenv.hostPlatform.system}.activationPackage}" "$out/${name}"''
-              ) (config.lib.attrs.filter (_: value: value.result ? ${stdenv.hostPlatform.system}) config.homes)
-            ));
-          };
-      };
-
-      packages.helix = {
-        systems = [ "x86_64-linux" ];
-
-        package =
-          { helix }:
-          helix.overrideAttrs (
-            {
-              patches ? [ ],
-              ...
-            }:
-            {
-              doCheck = false;
-              patches = patches ++ [ ./patches/helix/3958-labels-for-config-menus.patch ];
-            }
-          );
-      };
-
-      # With a package set defined, we can create a shell.
-      shells.default = {
-        # Declare what systems the shell can be used on.
-        systems = [ "x86_64-linux" ];
-
-        # Define our shell environment.
-        shell =
-          {
-            pkgs,
-            stdenv,
-            mkShell,
-            kdePackages,
-            reuse,
-            ...
-          }:
-          mkShell {
-            QML_IMPORT_PATH =
-              lib.fp.pipe
-                [
-                  (map (pkg: "${pkg}/lib/qt-6/qml"))
-                  (builtins.concatStringsSep ":")
-                ]
-                [
-                  config.inputs.nixos-unstable.result.${stdenv.hostPlatform.system}.quickshell
-                  kdePackages.qtdeclarative
-                ];
-
-            packages = [
-              config.inputs.nilla-cli.result.packages.nilla-cli.result.${stdenv.hostPlatform.system}
-              config.inputs.nilla-home.result.packages.nilla-home.result.${stdenv.hostPlatform.system}
-              config.inputs.nilla-nixos.result.packages.nilla-nixos.result.${stdenv.hostPlatform.system}
-              config.inputs.nixos-unstable.result.${stdenv.hostPlatform.system}.quickshell
-              config.inputs.nixpkgs.result.${stdenv.hostPlatform.system}.deadnix
-              config.packages.nilla-fmt.result.${stdenv.hostPlatform.system}
-              config.packages.treefmt.result.${stdenv.hostPlatform.system}
-              (config.inputs.npins.result {
-                inherit pkgs;
-                inherit (stdenv.hostPlatform) system;
-              })
-              kdePackages.qtdeclarative
-              reuse
-            ];
-          };
-      };
+    args = {
+      inherit nilla pins; # pins needs to be a static arg for us to import from it...
     };
-  }
-)
+  };
+in
+result.config
+// {
+  extend = result.extend;
+}
