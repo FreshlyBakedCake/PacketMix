89db921d889d1f8a1eed81a9afbdaccddb11265a
Skyler Grey <minion@freshlybakedca.ke>
2026-07-17 11:24:40 +0200

    feat(pm/server): set up backups
    We need backups on our servers again. We have a Hetzner storage box - we
    should make the servers run backups automatically every day

---
+++ packetmix/systems/server/backups.nix
@@ -0,0 +1,13 @@
+{ config, ... }: {
+  services.borgbackup.jobs.persist = {
+    paths = "/persist/data";
+    encryption = {
+      mode = "repokey";
+      passCommand = "cat /secrets/backups/repo-passphrase.txt";
+    };
+    repo = "ssh://backups/./borg";
+    compression = "auto,zstd";
+    startAt = "*-*-* 01:00:00 Etc/UTC";
+    persistentTimer = true;
+  };
+}
---
+++ packetmix/systems/teal/backups.nix
@@ -0,0 +1,11 @@
+{
+  programs.ssh.extraConfig = ''
+    Host backups
+      HostName u526469-sub1.your-storagebox.de
+      User u526469-sub1
+      IdentityFile /secrets/backups/id_ed25519
+      Port 23
+  '';
+
+  programs.ssh.knownHosts."[u526469-sub1.your-storagebox.de]:23".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIICf9svRenC/PLKIL9nk6K/pxQgoiFC41wTNvoIncOxs";
+}
